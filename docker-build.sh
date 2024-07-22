b232acf6c2b94e606ef2c91440daed058fecfbcf
sue-CC <caishutong26@gmail.com>
2024-07-22 18:34:25 +0200

    disable duplicate check

--- docker-build.sh
+++ docker-build.sh
@@ -18,31 +18,3 @@ docker buildx build --platform linux/amd64,linux/arm64 --tag $DOCKER_ACCOUNT/pig
 # Remove the builder instance
 docker buildx rm mybuilder
 
-
-##!/bin/sh
-#if [ -z "$DOCKER_ACCOUNT" ]; then
-#    DOCKER_ACCOUNT=clytze
-#fi;
-#docker build --tag=piggy-account-grpc account-service
-#docker tag piggy-account-grpc $DOCKER_ACCOUNT/piggy-account-grpc:latest
-#docker push $DOCKER_ACCOUNT/piggy-account-grpc:latest
-#
-#docker build --tag=piggy-auth-grpc auth-service
-#docker tag piggy-auth-grpc $DOCKER_ACCOUNT/piggy-auth-grpc:latest
-#docker push $DOCKER_ACCOUNT/piggy-auth-grpc
-#
-#docker build --tag=piggy-notification-grpc notification-service
-#docker tag piggy-notification-grpc $DOCKER_ACCOUNT/piggy-notification-grpc:latest
-#docker push $DOCKER_ACCOUNT/piggy-notification-grpc
-#
-#docker build --tag=piggy-registry registry
-#docker tag piggy-registry $DOCKER_ACCOUNT/piggy-registry:latest
-#docker push $DOCKER_ACCOUNT/piggy-registry
-#
-#docker build --tag=piggy-statistics-grpc statistics-service
-#docker tag piggy-statistics-grpc $DOCKER_ACCOUNT/piggy-statistics-grpc:latest
-#docker push $DOCKER_ACCOUNT/piggy-statistics-grpc
-#
-#docker build --tag=piggy-mongodb mongodb
-#docker tag piggy-mongodb $DOCKER_ACCOUNT/piggy-mongodb:latest
-#docker push $DOCKER_ACCOUNT/piggy-mongodb
