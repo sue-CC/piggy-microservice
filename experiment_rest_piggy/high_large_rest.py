a10d7033fef8689057980ef6b7145c95a70f9481
sue-CC <caishutong26@gmail.com>
2024-07-30 00:19:33 +0200

    scripts done

---
+++ experiment_rest_piggy/high_large_rest.py
@@ -0,0 +1,219 @@
+import secrets
+
+import logging
+import random
+import time
+import os
+
+from threading import Lock, Timer
+
+import rest_user
+from rest_user import RestUser
+from locust import between, task
+
+logging.basicConfig(level=logging.INFO)
+
+# Define environment variables
+FREQUENCY_LEVEL = os.getenv('FREQUENCY_LEVEL', 'high')
+SIZE_LEVEL = os.getenv('SIZE_LEVEL', 'large')
+
+# Set mappings
+FREQUENCY_MAPPING = {'low': 8, 'medium': 80, 'high': 400}
+SIZE_MAPPING = {'small': 50, 'medium': 1000, 'large': 100000}
+
+global_max_requests = FREQUENCY_MAPPING[FREQUENCY_LEVEL]
+payload_size = SIZE_MAPPING[SIZE_LEVEL]
+
+max_requests = {"create_account": 400,
+                "get_account": 400,
+                "update_account": 400,
+                "get_auth": 400,
+                "update_auth": 400,
+                "post_auth": 400,
+                "get_statistics": 400,
+                "update_statistics": 400,
+                "get_recipient": 400,
+                "update_recipient": 400
+                }
+total_requests = {name: 0 for name in max_requests.keys()}
+
+
+class AccountServiceTasks(rest_user.RestUser):
+    wait_time = between(1, 3)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.request_counts = None
+        self.locks = {name: Lock() for name in [
+            "create_account", "get_account", "update_account",
+            "get_auth", "update_auth", "post_auth",
+            "get_statistics", "update_statistics",
+            "get_recipient", "update_recipient"
+        ]}
+        self.created_usernames = ['Tom111']
+        self.created_users = ['Tom111']
+
+    def _increment_request_count(self, task_name):
+        with self.locks[task_name]:
+            if total_requests[task_name] < global_max_requests:
+                total_requests[task_name] += 1
+                return True
+            return False
+
+    def _generate_unique_username(self):
+        while True:
+            username = f"{random.randint(0, 999999):06}"
+            if username not in self.created_usernames:
+                self.created_usernames.append(username)
+                return username
+
+    def _generate_unique_users(self):
+        while True:
+            username = f"{random.randint(0, 999999):06}"
+            if username not in self.created_users:
+                self.created_users.append(username)
+                return username
+
+    @task
+    def execute_tasks_in_sequence(self):
+        self.create_account()
+        self.update_account()
+        self.get_account()
+        self.update_statistics()
+        self.get_statistics()
+        self.update_recipient()
+        self.get_recipient()
+        self.create_user()
+        self.update_user()
+
+    def create_account(self):
+        with self.locks["create_account"]:
+            if total_requests["create_account"] < global_max_requests * 2:
+                total_requests["create_account"] += 1
+                username = f"{random.randint(0, 999999):06}"
+                password = secrets.token_bytes(49981).hex()
+                payload = {
+                    "username": username,
+                    "password": password
+                }
+                try:
+                    self.make_request("/accounts/", "POST", payload=payload, task_name="account")
+                    self.created_usernames.append(username)
+                except Exception as e:
+                    logging.error(f"Failed to create account: {e}")
+
+    def update_account(self):
+        if self._increment_request_count("update_account"):
+            username = f"{random.randint(0, 999999):06}"
+            amount = secrets.token_bytes(16619).hex()
+            payload = {
+                "incomes": [{"title": "Salary111", "amount": amount, "currency": "EUR", "period": "MONTH"}],
+                "expenses": [{"title": "Rent22", "amount": amount, "currency": "EUR", "period": "MONTH"}],
+                "saving": {"amount": amount, "currency": "EUR", "interest": "2.5", "deposit": "true", "capitalization": "false"}
+            }
+            try:
+                self.make_request(f"/accounts/{username}", method="PUT", payload=payload, task_name="account")
+                self.created_usernames.append(username)
+            except Exception as e:
+                logging.error(f"Failed to update account: {e}")
+
+    def get_account(self):
+        if self._increment_request_count("get_account") and self.created_usernames:
+            username = random.choice(self.created_usernames)
+            try:
+                self.make_request(f"/recipients/{username}", method="GET", task_name="account")
+            except Exception as e:
+                logging.error(f"Failed to get account: {e}")
+
+    def update_statistics(self):
+        if self._increment_request_count("update_statistics"):
+            amount = secrets.token_bytes(12455).hex()
+            payload = {
+                "incomes": [
+                    {"title": "Rent11", "amount": amount, "currency": "EUR", "period": "MONTH"},
+                    {"title": "Freelance1", "amount": amount, "currency": "EUR", "period": "MONTH"}
+                ],
+                "expenses": [{"title": "Rent22", "amount": amount, "currency": "EUR", "period": "MONTH"}],
+                "saving": {"amount": amount, "currency": "USD", "interest": "2.5", "deposit": "true", "capitalization": "false"}
+            }
+            try:
+                self.make_request(f"/statistics/{random.choice(self.created_usernames)}",
+                                  method="PUT",
+                                  payload=payload,
+                                  task_name="statistics"
+                                  )
+            except Exception as e:
+                logging.error(f"Failed to get account: {e}")
+
+    def get_statistics(self):
+        if self._increment_request_count("get_statistics"):
+            self.make_request(f"/statistics/{random.choice(self.created_usernames)}",payload=None, method="GET", task_name="statistics")
+
+    def update_recipient(self):
+        if self._increment_request_count("update_recipient"):
+            username = f"{random.randint(0, 999999):06}"
+            email = secrets.token_bytes(49911).hex()
+            payload = {
+                    "accountName": username,
+                    "email": email+"@gmail.com",
+                    "scheduledNotifications": {
+                    "BACKUP": {"active": "true", "frequency": "HIGH"},
+                    "REMIND": {"active": "false", "frequency": "LOW"}
+                }
+            }
+            self.make_request(f"/recipients/{username}", method="PUT", payload=payload, task_name="recipient")
+
+    def get_recipient(self):
+        if self._increment_request_count("get_recipient"):
+            username = random.choice(self.created_usernames)
+            self.make_request(f"/recipients/{username}", method="GET", port=8083, task_name="recipient")
+
+    def create_user(self):
+        if self._increment_request_count("post_auth"):
+            username = f"{random.randint(0, 999999):06}"
+            password = secrets.token_bytes(49981).hex()
+            payload = {
+                "username": username,
+                "password": password
+            }
+            self.make_request(f"/users", method="POST", payload=payload, port=5000, task_name="auth")
+
+    def update_user(self):
+        if self._increment_request_count("update_auth"):
+            username = f"{random.randint(0, 999999):06}"
+            password = secrets.token_bytes(49981).hex()
+            payload = {
+                "username": username,
+                "password": password
+            }
+            self.make_request(f"/users", method="PUT", payload=payload, port=5000, task_name="auth")
+
+
+class WebsiteUser(RestUser):
+    tasks = [AccountServiceTasks]
+    wait_time = between(1, 3)
+    total_requests = {name: 0 for name in max_requests.keys()}
+
+    start_time = None
+    frequency_level = os.getenv('FREQUENCY_LEVEL', 'low')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.stop_timer = None
+        self.results = []
+
+    def on_start(self):
+        self.results = []
+        self.start_time = time.time()
+        self.stop_timer = Timer(150, self.stop_user)
+
+        self.stop_timer.start()
+
+    def on_stop(self):
+        if self.stop_timer and self.stop_timer.is_alive():
+            self.stop_timer.cancel()
+            self.stop_timer.join()
+
+    def stop_user(self):
+        self.environment.runner.quit()
+        logging.info("Test stopped after 2 min 30s")
---
+++ experiment_rest_piggy/rest_user.py
@@ -0,0 +1,89 @@
+import logging
+import time
+import uuid
+import requests
+from locust import User
+from locust.exception import LocustError
+from typing import Any, Callable
+
+class RestInterceptor:
+    def __init__(self, environment, *args, **kwargs):
+        self.env = environment
+
+    def intercept(self, method: Callable, method_name: str, endpoint: str, url: str, **kwargs: Any):
+        request_id = str(uuid.uuid4())
+        response = None
+        exception = None
+        start_perf_counter = time.perf_counter()
+        response_length = 0
+        request_size = 0
+
+        try:
+            # Calculate the size of the request payload
+            request_data = kwargs.get("data") or kwargs.get("json")
+            if request_data:
+                request_size = len(request_data.encode('utf-8')) if isinstance(request_data, str) else len(str(request_data).encode('utf-8'))
+
+            # Include headers size in the request size
+            headers = kwargs.get("headers", {})
+            for header, value in headers.items():
+                request_size += len(header.encode('utf-8')) + len(value.encode('utf-8')) + 4  # 4 is for ': ' and '\r\n'
+
+            response = method(url, **kwargs)
+            response_length = len(response.content)
+        except requests.RequestException as e:
+            exception = e
+            response = e.response
+
+        response_time = (time.perf_counter() - start_perf_counter) * 1000
+
+        self.env.events.request.fire(
+            request_type="http",
+            name=f"{method_name} {endpoint} {request_id}",
+            response_time=response_time,
+            response_length=request_size,
+            context=None,
+            exception=exception,
+            # request_size=request_size
+        )
+
+        return response
+
+class RestUser(User):
+    abstract = True
+    hosts = {
+        "account": 6000,
+        "auth": 5000,
+        "recipient": 8083,
+        "statistics": 7000,
+    }
+
+    def __init__(self, environment):
+        super().__init__(environment)
+        self.rest_interceptor = RestInterceptor(environment)
+
+    def make_request(self, endpoint, method="GET", payload=None, task_name=None, port=None):
+        if task_name:
+            port = self.hosts.get(task_name, port)
+        if not port:
+            raise LocustError(f"No port specified for task {task_name} and no port provided.")
+
+        url = f"http://145.108.225.14:{port}{endpoint}"
+        response = None
+        try:
+            method_func = self._get_method_func(method)
+            if method_func:
+                response = self.rest_interceptor.intercept(method_func, method, endpoint, url, json=payload)
+        except Exception as e:
+            logging.error(f"Exception during request: {str(e)}")
+
+        return response
+
+    def _get_method_func(self, method):
+        method_map = {
+            "GET": requests.get,
+            "POST": requests.post,
+            "PUT": requests.put,
+            "DELETE": requests.delete
+        }
+        return method_map.get(method)
