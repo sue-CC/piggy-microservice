a235cbe5236b9c2456ed08905a749691e8294b5c
sue-CC <caishutong26@gmail.com>
2024-07-29 23:41:24 +0200

    add password for mongodb

--- docker-build.sh
+++ docker-build.sh
@@ -8,11 +8,11 @@ docker buildx create --name mybuilder --use
 docker buildx inspect --bootstrap
 
 # Build and push for multiple architectures
-docker buildx build --platform linux/amd64,linux/arm64 --tag $DOCKER_ACCOUNT/piggy-account-grpc:latest --push account-service
-docker buildx build --platform linux/amd64,linux/arm64 --tag $DOCKER_ACCOUNT/piggy-auth-grpc:latest --push auth-service
-docker buildx build --platform linux/amd64,linux/arm64 --tag $DOCKER_ACCOUNT/piggy-notification-grpc:latest --push notification-service
-docker buildx build --platform linux/amd64,linux/arm64 --tag $DOCKER_ACCOUNT/piggy-registry:latest --push registry
-docker buildx build --platform linux/amd64,linux/arm64 --tag $DOCKER_ACCOUNT/piggy-statistics-grpc:latest --push statistics-service
+#docker buildx build --platform linux/amd64,linux/arm64 --tag $DOCKER_ACCOUNT/piggy-account-grpc:latest --push account-service
+#docker buildx build --platform linux/amd64,linux/arm64 --tag $DOCKER_ACCOUNT/piggy-auth-grpc:latest --push auth-service
+#docker buildx build --platform linux/amd64,linux/arm64 --tag $DOCKER_ACCOUNT/piggy-notification-grpc:latest --push notification-service
+#docker buildx build --platform linux/amd64,linux/arm64 --tag $DOCKER_ACCOUNT/piggy-registry:latest --push registry
+#docker buildx build --platform linux/amd64,linux/arm64 --tag $DOCKER_ACCOUNT/piggy-statistics-grpc:latest --push statistics-service
 docker buildx build --platform linux/amd64,linux/arm64 --tag $DOCKER_ACCOUNT/piggy-mongodb:latest --push mongodb
 
 # Remove the builder instance
--- mongodb/init.sh
+++ mongodb/init.sh
@@ -1,22 +1,38 @@
 #!/bin/bash
 
-# Remove authentication-related logic and database renaming
 echo "Starting MongoDB setup..."
 
 # INIT DUMP EXECUTION
 (
 if test -n "$INIT_DUMP"; then
     echo "Executing dump file on the piggy database"
-    until mongo piggy $INIT_DUMP; do sleep 5; done
+    until mongo piggy -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD --authenticationDatabase admin $INIT_DUMP; do sleep 5; done
 fi
 ) &
 
 echo "Start MongoDB"
 chown -R mongodb /data/db
-gosu mongodb mongod "$@"
+gosu mongodb mongod "$@" &
 
 # Wait for MongoDB to initialize
-sleep 5
+sleep 10
+
+echo "Creating MongoDB users"
+mongo <<EOF
+use admin
+db.createUser({
+  user: '$MONGO_INITDB_ROOT_USERNAME',
+  pwd: '$MONGO_INITDB_ROOT_PASSWORD',
+  roles: [ { role: 'userAdminAnyDatabase', db: 'admin' }, 'readWriteAnyDatabase' ]
+})
+
+use $MONGO_INITDB_DATABASE
+db.createUser({
+  user: '$MONGO_INITDB_USERNAME',
+  pwd: '$MONGO_INITDB_PASSWORD',
+  roles: [ { role: 'readWrite', db: '$MONGO_INITDB_DATABASE' } ]
+})
+EOF
 
 echo "MongoDB started successfully"
 exec gosu mongodb /usr/local/bin/docker-entrypoint.sh "$@"
